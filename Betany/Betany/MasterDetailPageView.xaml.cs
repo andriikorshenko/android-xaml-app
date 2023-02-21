6c2365e2a336fda5ebb32246e6a1ed1751998a8e
andriikorshenko <andriikorshenko@gmail.com>
2023-02-21 15:41:45 +0100

    Add MasterPageView

---
+++ Betany/Betany/MasterDetailPageView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Betany
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPageView : MasterDetailPage
+    {
+        public MasterDetailPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Betany/Betany/MasterNavigationItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Betany
+{
+    public class MasterNavigationItem
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public Type Target { get; set; }
+    }
+}
