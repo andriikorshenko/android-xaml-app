f3772fd3a7066b460b0be0bbea2393c01994432c
andriikorshenko <andriikorshenko@gmail.com>
2023-02-21 16:14:41 +0100

    Fix bags

--- Betany/Betany/obj/Debug/netstandard2.0/MasterDetailPageView.xaml.g.cs
+++ Betany/Betany/obj/Debug/netstandard2.0/MasterDetailPageView.xaml.g.cs
@@ -14,11 +14,15 @@ namespace Betany {
     
     
     [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("MasterDetailPageView.xaml")]
-    public partial class MasterDetailPageView : global::Xamarin.Forms.ContentPage {
+    public partial class MasterDetailPageView : global::Xamarin.Forms.MasterDetailPage {
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Betany.Helpers.MasterPageView masterView;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MasterDetailPageView));
+            masterView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Betany.Helpers.MasterPageView>(this, "masterView");
         }
     }
 }
